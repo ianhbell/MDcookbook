4f3bdd810dfb4390922fc9a868460f0ea974a1bc
Ian Bell <ian.bell@nist.gov>
2019-11-02 20:57:17 -0600

    Set variables from command line

--- LAMMPS/3Ddviscosityexamples/go.sh
+++ LAMMPS/3Ddviscosityexamples/go.sh
@@ -1,2 +1,2 @@
 docker build -t lam3d .
-docker run -v "${PWD}":/output -d lam3d bash -c "cd /output && mpirun -np 12 --allow-run-as-root lammps -sf opt -in LJ_GK.3d"
+docker run -v "${PWD}":/output -d lam3d bash -c "cd /output && mpirun -np 12 --allow-run-as-root lammps -sf opt -in LJ_GK.3d -var rhostar 0.3 -var Tstar 1.3"
