da074663d4b73cca2dd34ea9b3ebcd6260d98cf4
Ian Bell <ian.bell@nist.gov>
2020-01-23 13:45:02 -0700

    Fix RDF runner path

--- RUMD/LJ/rdf/go.sh
+++ RUMD/LJ/rdf/go.sh
@@ -1,1 +1,1 @@
-pyflakes check_RDF.py && docker run --gpus device=1 -v "${PWD}":/output -d rumd bash -c "cd /output && python check_RDF.py"
+pyflakes check_RDF.py && cd .. && docker run --gpus device=1 -v "${PWD}":/output -d rumd bash -c "cd /output/rdf && python check_RDF.py"
